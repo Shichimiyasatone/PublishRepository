4bbc0163eab269349c9947744da61cce3b9d0b21
Shichimiyasatone <35890954+Shichimiyasatone@users.noreply.github.com>
2018-09-24 10:24:32 +0800

    Tanks_Project
    unity3D坦克世界

---
+++ Tanks_Project/Assets/Scripts/TankAttack.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TankAttack : NetworkBehaviour {
+
+    public GameObject shellPrefab;
+    public KeyCode fireKey = KeyCode.Space;
+    public float shellSpeed = 5;
+    public AudioClip shotAudio;
+
+    private Transform firePosition;
+
+    // Use this for initialization
+    void Start() {
+        firePosition = transform.Find("FirePositon");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(fireKey))
+        {
+            //Debug.Log("Fire!");
+            CmdLanPlayerFire();
+        }
+    }
+
+    void SinglePlayerFire()
+    {    
+            GameObject go = GameObject.Instantiate(shellPrefab, firePosition.position, firePosition.rotation) as GameObject;
+            go.GetComponent<Rigidbody>().velocity = go.transform.forward * shellSpeed;
+            AudioSource.PlayClipAtPoint(shotAudio, transform.position); 
+    }
+
+    [Command]
+    void CmdLanPlayerFire()
+    {
+        GameObject go = GameObject.Instantiate(shellPrefab, firePosition.position, firePosition.rotation) as GameObject;
+        go.GetComponent<Rigidbody>().velocity = go.transform.forward * shellSpeed;
+        AudioSource.PlayClipAtPoint(shotAudio, transform.position);
+        NetworkServer.Spawn(go);
+    }
+}
---
+++ Tanks_Project/Assets/TelescopicView.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TelescopicView : MonoBehaviour
+{
+
+    //公有成员
+
+    public float ZoomLevel = 2.0f;
+
+    public float ZoomInSpeed = 100.0f;
+
+    public float ZoomOutSpeed = 100.0f;
+
+    //私有成员
+
+    private float initFOV;
+
+    //脚本初始化时，调用此函数
+
+    void Start()
+
+    {
+
+        //获取当前摄像机的视野范围
+
+        initFOV = Camera.main.fieldOfView;
+        //Debug.Log("初始化FOV=" + initFOV);
+    }
+
+    //运行游戏时的每一帧，都调用此函数
+
+    void Update()
+
+    {
+
+        //当鼠标左键按下时
+
+        if (Input.GetKey(KeyCode.Mouse0))
+
+        {
+
+            ZoomView();
+
+        }
+
+        else
+
+        {
+
+            ZoomOut();
+
+        }
+
+    }
+
+    //放大摄像机的视野区域
+
+    void ZoomView()
+
+
+    {
+        //Debug.Log("触发ZoomView");
+
+        if (Mathf.Abs(Camera.main.fieldOfView - (initFOV / ZoomLevel)) < 0.5f)
+
+        {
+
+            Camera.main.fieldOfView = initFOV / ZoomLevel;
+
+        }
+
+        else if (Camera.main.fieldOfView - (Time.deltaTime * ZoomInSpeed) >= (initFOV / ZoomLevel))
+
+        {
+
+            Camera.main.fieldOfView -= (Time.deltaTime * ZoomInSpeed);
+
+        }
+
+    }
+
+    //缩小摄像机的视野区域
+
+    void ZoomOut()
+
+    {
+        //Debug.Log("触发ZoomOut");
+        if (Mathf.Abs(Camera.main.fieldOfView - initFOV) < 0.5f)
+
+        {
+
+            Camera.main.fieldOfView = initFOV;
+
+        }
+
+        else if (Camera.main.fieldOfView + (Time.deltaTime * ZoomOutSpeed) <= initFOV)
+        {
+
+            Camera.main.fieldOfView += (Time.deltaTime * ZoomOutSpeed);
+
+        }
+
+    }
+}
